c05fbddc25367da219008c0a3f60137ffd603382
Nilesh Chakraborty <nilesh@nileshc.com>
2015-03-06 00:53:58 +0000

    Added big sem interpreter

--- sm/persist.py
+++ sm/persist.py
@@ -4,4 +4,4 @@ class Persist():
     query = None
     authPubs = None
     dblpAuthorsList = None
-    matcher = {'emltex_selected': None, 'emltex': None, 'emltexwiki_selected': None, 'emltexwiki': None}
+    matcher = {'emltex_selected': None, 'emltex': None, 'emltexwiki_selected': None, 'emltexwiki': None, 'emlallwiki': None}
